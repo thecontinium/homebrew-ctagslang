6fbeb6a7fdbc05fea09c6153296bae69d1b24c00
thecontinium <thecontinium@outlook.com>
2016-11-21 19:36:22 +0800

    Put additional flags before the path

--- Formula/ctagspython.rb
+++ Formula/ctagspython.rb
@@ -1,7 +1,7 @@
 class Ctagspython < Formula
   homepage 'https://github.com/thecontinium/homebrew-ctagspython/'
   url 'https://github.com/thecontinium/homebrew-ctagspython.git'
-  version '1.0.4'
+  version '1.0.5'
 
   skip_clean 'bin'
 
