705287a9814bfbaf3ed6f5e26d5c1f5a68d65067
thecontinium <thecontinium@outlook.com>
2016-12-12 14:38:27 +0800

    install vim ctags

--- Formula/ctagspython.rb
+++ Formula/ctagspython.rb
@@ -1,12 +1,13 @@
 class Ctagspython < Formula
   homepage 'https://github.com/thecontinium/homebrew-ctagspython/'
   url 'https://github.com/thecontinium/homebrew-ctagspython.git'
-  version '1.0.6'
+  version '1.0.7'
 
   skip_clean 'bin'
 
   def install
     bin.install 'ctags_python'
+    bin.install 'ctags_vim'
   end
 
 end
